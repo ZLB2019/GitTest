8da004c0aa66c2d7bc31c8e9e80e47958b1e284c
ZLB2019 <1059954375@qq.com>
2021-10-07 15:24:33 +0800

    first commit

---
+++ src/main/java/ZLB.java
@@ -0,0 +1,11 @@
+/**
+ * @author ZLB
+ * @version 1.0
+ * @date 2021/10/7 15:15
+ * @annotations
+ */
+public class ZLB {
+    public static void main(String[] args) {
+        System.out.println("ZLBNB");
+    }
+}
